bf64bf8f225c7b150475864e22af110d6712c68a
i-havr <i.havrylov@gmail.com>
2023-11-17 20:45:23 +0200

    add follows

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -5,9 +5,10 @@ import { UserEntity } from '@app/user/user.entity';
 
 import { ProfileController } from '@app/profile/profile.controller';
 import { ProfileService } from '@app/profile/profile.service';
+import { FollowEntity } from './follow.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, FollowEntity])],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
